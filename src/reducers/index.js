d96844a1c7ec98f979e00c45591eb8fd35f35145
shantanu pandey <shantanupandey00@gmail.com>
2021-11-04 12:00:29 -0400

    Updated reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,18 @@
+
 import { combineReducers } from "redux";
 import auth from './auth';
 import search from './search';
 import food from './food';
 import history from './history';
-import job from './job'
+import job from './job';
+import application from './application'
 
 export default combineReducers({
     auth,
     search,
     food,
     history,
-    job
+    job,
+    application
 })
+
