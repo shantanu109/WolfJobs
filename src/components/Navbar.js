10e6edcea82106a9900ea5324ac3a3e69d5325b8
shantanu pandey <shantanupandey00@gmail.com>
2021-11-04 11:47:42 -0400

    Updated the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,50 @@
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import React from 'react';
-import { logoutUser } from '../actions/auth';
 
-// import { searchUsers } from '../actions/search';
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
-import FlagIcon from '@material-ui/icons/Flag';
-import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
-import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
-import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import React from "react";
+import { logoutUser } from "../actions/auth";
+
+import { searchUsers } from "../actions/search";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
 
 class Navbar extends React.Component {
   logOut = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     this.props.dispatch(logoutUser());
   };
 
-//   handleSearch = (e) => {
-//     const searchText = e.target.value;
+  handleSearch = (e) => {
+    const searchText = e.target.value;
 
-//     this.props.dispatch(searchUsers(searchText));
-//   };
+    this.props.dispatch(searchUsers(searchText));
+  };
 
-  
   render() {
     const { auth } = this.props;
-    const {user} = this.props.auth;
+    const { user } = this.props.auth;
 
     return (
-      
       <nav className="header">
-        <div className="header__left" style={{display:'flex',justifyContent:'space-evenly'}}>
+        <div
+          className="header__left"
+          style={{ display: "flex", justifyContent: "space-evenly" }}
+        >
           <Link to="/">
             <img
-            // http://ninjasfiles.s3.amazonaws.com/0000000000003454.png
-            //   src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+              // http://ninjasfiles.s3.amazonaws.com/0000000000003454.png
+              //   src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
               alt="logo"
               src="/images/burnlogo2.jpeg"
-              style={{height:'50px'}}
-
+              style={{ height: "50px" }}
             />
           </Link>
-        
-        {/* <div className="header__input"> */}
+
+          {/* <div className="header__input"> */}
           {/* <img
             className="search-icon"
             src="https://image.flaticon.com/icons/svg/483/483356.svg"
@@ -68,73 +69,136 @@ class Navbar extends React.Component {
               </ul>
             </div>
           )} */}
-        {/* </div> */}
+          {/* </div> */}
+          <div className="header__input">
+            {/* <img
+            className="search-icon"
+            src="https://image.flaticon.com/icons/svg/483/483356.svg"
+            alt="search-icon"
+          /> */}
+            <SearchIcon />
+            <input
+              placeholder="Search"
+              onChange={this.handleSearch}
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                outlineWidth: "0",
+                width: "500px",
+              }}
+            />
+          </div>
         </div>
-        {auth.isLoggedIn  &&
-        <div className="header__middle" style={{marginLeft:'70px'}}>
-          {user.role == 'Manager' && 
-          <div className="header__option ">
-          <Link to="/">
-          <HomeIcon fontSize="large"/>
-          </Link>
-        </div>}
-          
-          <div className="header__option ">
-            <Link to="/goal">
-            <FlagIcon fontSize="large"/>
-            </Link>
-          </div>
-           <div className="header__option ">
-             <Link to="/history">
-            <SubscriptionsOutlinedIcon fontSize="large"/>
-            </Link>
-          </div>
-          {/* <div className="header__option">
+        {auth.isLoggedIn && (
+          <div className="header__middle" style={{ marginLeft: "70px" }}>
+            <div className="header__option ">
+              <Link to="/">
+                <HomeIcon fontSize="large" />
+              </Link>
+            </div>
+            {user.role == "Manager" && (
+              <div className="header__option ">
+                <Link to="/goal">
+                  <FlagIcon fontSize="large" />
+                </Link>
+              </div>
+            )}
+            {user.role == "Manager" && (
+              <div className="header__option ">
+                <Link to="/history">
+                  <SubscriptionsOutlinedIcon fontSize="large" />
+                </Link>
+              </div>
+            )}
+            {user.role == "Applicant" && (
+              <div className="header__option ">
+                <Link to="/applicationinfo">
+                  <SubscriptionsOutlinedIcon fontSize="large" />
+                </Link>
+              </div>
+            )}
+
+            {/* <div className="header__option">
             <StorefrontOutlinedIcon fontSize="large"/>
           </div>
           <div className="header__option">
             <SupervisedUserCircleIcon fontSize="large"/>
           </div>  */}
-         </div> }
+          </div>
+        )}
         <div className="header__right">
-        <div className="header__info">
-          {auth.isLoggedIn && (
-            <div className="user">
-              <Link to="/settings">
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-dp"
-                  id="user-dp"
-                  style={{marginLeft:'0px'}}
-                />
-              </Link>
-              <span style={{color:'gray',marginLeft:'10px',fontWeight:'bolder'}}>{auth.user.name}</span>
-              {/* <span style={{color:'gray',marginLeft:'10px',fontWeight:'bolder'}}>XYZ</span> */}
-            </div>
-          )} 
+          <div className="header__info">
+            {auth.isLoggedIn && (
+              <div className="user">
+                <Link to="/settings">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                    id="user-dp"
+                    style={{ marginLeft: "0px" }}
+                  />
+                </Link>
+                <span
+                  style={{
+                    color: "gray",
+                    marginLeft: "10px",
+                    fontWeight: "bolder",
+                  }}
+                >
+                  {auth.user.name}
+                </span>
+                {/* <span style={{color:'gray',marginLeft:'10px',fontWeight:'bolder'}}>XYZ</span> */}
+              </div>
+            )}
 
-          <div className="nav-links" >
-            <ul>
-              {!auth.isLoggedIn && (
-                <li>
-                  <Link to="/login" style={{color:'gray',marginLeft:'10px',fontWeight:'bolder'}}>Login</Link>
-                </li>
-              )} 
-              
-              {auth.isLoggedIn && 
-              <li onClick={this.logOut} style={{color:'gray',marginLeft:'0px',fontWeight:'bolder'}}>Logout</li> 
-               }
-              {!auth.isLoggedIn && (
-                <li>
-                  <Link to="/signup" style={{color:'gray',marginLeft:'10px',fontWeight:'bolder'}} >Register</Link>
-                </li>
-              )}
-            </ul>
+            <div className="nav-links">
+              <ul>
+                {!auth.isLoggedIn && (
+                  <li>
+                    <Link
+                      to="/login"
+                      style={{
+                        color: "gray",
+                        marginLeft: "10px",
+                        fontWeight: "bolder",
+                      }}
+                    >
+                      Login
+                    </Link>
+                  </li>
+                )}
+
+                {auth.isLoggedIn && (
+                  <li
+                    onClick={this.logOut}
+                    style={{
+                      color: "gray",
+                      marginLeft: "0px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    Logout
+                  </li>
+                )}
+                {!auth.isLoggedIn && (
+                  <li>
+                    <Link
+                      to="/signup"
+                      style={{
+                        color: "gray",
+                        marginLeft: "10px",
+                        fontWeight: "bolder",
+                      }}
+                    >
+                      Register
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
           </div>
         </div>
-        </div>
       </nav>
-      
     );
   }
 }
@@ -150,5 +214,6 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Navbar);
+
 
 
