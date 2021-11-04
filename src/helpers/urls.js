8a39e3532f777b34405e18c8978bbdb98512bfc4
Sahil-Nande <sahilnande@gmail.com>
2021-11-03 23:16:49 -0400

    Added Urls for making the API calls to the backend

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -5,12 +5,18 @@ export const APIURLS = {
 
     login: () => `${API_ROOT}/users/create-session`,
     signup: () => `${API_ROOT}/users/signup`,
-    // fetchPosts: (page=1, limit=25) => `${API_ROOT}/posts?page=${page}&limit=${limit}`,
+    fetchJobs: (page=1, limit=25) => `${API_ROOT}/users?page=${page}&limit=${limit}`,
     editProfile : () => `${API_ROOT}/users/edit`,
     editHistory : () => `${API_ROOT}/users/createhistory`,
     getHistory: (date,userId) => `${API_ROOT}/users/gethistory?id=${userId}&date=${date}`,
     createJob: () => `${API_ROOT}/users/createjob`,
+    createApplication: () => `${API_ROOT}/users/createapplication`,
+    acceptApplication: () => `${API_ROOT}/users/acceptapplication`,
+    rejectApplication: () => `${API_ROOT}/users/rejectapplication`,
     userSearch: (searchText) => `${API_ROOT}/users/search/${searchText}`,
+    fetchApplication: () => `${API_ROOT}/users/fetchapplications`,
+    closeJob: () => `${API_ROOT}/users/closejob`,
+
     // deletePost: (postId) => `${API_ROOT}/posts/destroy/${postId}`,
     // deleteComment: (commentId) => `${API_ROOT}/posts/destroycomment/${commentId}`
     
